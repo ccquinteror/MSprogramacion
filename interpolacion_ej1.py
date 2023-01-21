6f89a0362716c8690c42017ec06de11743f9830a
ccquinteror <109251741+ccquinteror@users.noreply.github.com>
2023-01-20 22:00:43 -0500

    act archivo base

--- interpolacion_ej1.py
+++ interpolacion_ej1.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.interpolate import lagrange
 
-# Se carga la base de datos del repositorio
+# Se carga la base de datos del repositorio archivo base actualizado
 ruta="https://github.com/ccquinteror/MSprogramacion/blob/main/data.xlsx?raw=true"
 data=pd.read_excel(ruta)
 
